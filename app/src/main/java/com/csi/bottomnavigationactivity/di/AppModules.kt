c882ee5b0d9e2c276f4d8c976e84b06f9e3e4b13
NKenges <nkenges@one.kz>
2022-11-10 23:41:13 +0600

    - viewModel fix

--- app/src/main/java/com/csi/bottomnavigationactivity/di/AppModules.kt
+++ app/src/main/java/com/csi/bottomnavigationactivity/di/AppModules.kt
@@ -6,12 +6,13 @@ import com.csi.bottomnavigationactivity.db.NoteDatabase
 import com.csi.bottomnavigationactivity.db.NotesDao
 import com.csi.bottomnavigationactivity.repository.NoteRepository
 import com.csi.bottomnavigationactivity.ui.home.HomeViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 
 val appModule = module {
     single { NoteRepository(get()) }
-    single { HomeViewModel(get()) }
+    viewModel { HomeViewModel(get()) }
 }
 
 val databaseModule = module {
